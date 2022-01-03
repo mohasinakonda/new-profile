8f6e94922c8db656a0dde7926fceb584a254bc40
mohasin akonda <mohasinakonda39@gmail.com>
2022-01-03 20:16:51 +0600

    something added

--- main.js
+++ main.js
@@ -13,11 +13,7 @@ let second_obj={
     passion:'be a human'
 }
 
-<<<<<<< HEAD
-let greet='hello world'
-=======
 let third_obj={
-    name:'Shojib Mahmud',
     email:'shojib.m@gmail.com',
     age:27,
     hobby:'pet a bird'
@@ -29,4 +25,3 @@ let bio={
     age:26,
     skill:['no','one','two']
 }
->>>>>>> first-branch
